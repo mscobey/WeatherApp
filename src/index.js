1cfcf069a735f8db87ce563dfacaa42d02d60232
mscobey <126064568+mscobey@users.noreply.github.com>
2023-04-30 16:41:00 -0500

    fix default gif,add catch

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ let location = document.getElementById('location');
 let weatherData;
 
 
+tempIcon.src="https://media1.giphy.com/media/QRhtqYeEywJI4/giphy.gif?cid=ecf05e47adsur9lsxnckwzndhmgjfgepipihtx2xh9as3egz&ep=v1_gifs_search&rid=giphy.gif&ct=g";
 
 
 locationBtn.addEventListener('click',getWeather);
@@ -27,27 +28,32 @@ celsiusBtn.addEventListener('change',()=>{
 });
 
 async function getWeather(){
-    scale = document.querySelector('input[name="tempscale"]:checked').value;
+    try{
+        scale = document.querySelector('input[name="tempscale"]:checked').value;
 
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3867dbef756147b0b14210801232804&q=${location.value}&aqi=no`,{mode:'cors'});
-    weatherData = await response.json();
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3867dbef756147b0b14210801232804&q=${location.value}&aqi=no`,{mode:'cors'});
+        weatherData = await response.json();
 
-    let description = weatherData.current.condition.text;
-    
-    let weatherLocation = weatherData.location.name;
+        let description = weatherData.current.condition.text;
+        
+        let weatherLocation = weatherData.location.name;
 
-    const gifResponse = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=WpKcZhie5SWn9qBuFg5FTbSRjI5MJQyD&s=${description}`, {mode: 'cors'});
-    const gifData = await gifResponse.json();
+        const gifResponse = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=WpKcZhie5SWn9qBuFg5FTbSRjI5MJQyD&s=${description}`, {mode: 'cors'});
+        const gifData = await gifResponse.json();
 
-    
-    tempIcon.src=gifData.data.images.original.url;
-    tempDesc.innerHTML=description;
-    
-    if(scale=='F'){
-        tempText.innerHTML=`The weather in ${weatherLocation} is: ${weatherData.current.temp_f}°F`;
-    }
-    else{
-        tempText.innerHTML=`The weather in ${weatherLocation} is: ${weatherData.current.temp_c}°C`;
+        
+        tempIcon.src=gifData.data.images.original.url;
+        tempDesc.innerHTML=description;
+        
+        if(scale=='F'){
+            tempText.innerHTML=`The weather in ${weatherLocation} is: ${weatherData.current.temp_f}°F`;
+        }
+        else{
+            tempText.innerHTML=`The weather in ${weatherLocation} is: ${weatherData.current.temp_c}°C`;
+        }
+    }catch(error){
+        console.log(error);
+        document.getElementById("error").innerHTML="Error! Cannot find a GIF for that search term!";
     }
 };
 
